de156446e381653531d06e3b3d92df037dd99cc7
Lalith aditya Chunduri <chun8135@vandals.uidaho.edu>
2025-01-12 13:56:54 -0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-let apiKey = "1e3e8f230b6064d27976e41163a82b77";
+let apiKey = "36ffc6ea6c048bb0fcc1752338facd48";
 let searchinput = document.querySelector(`.searchinput`);
 
 async function search(city, state, country){
